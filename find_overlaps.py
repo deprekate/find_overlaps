7a2483211be50258bd9af54d6ef18caaab948b8e
deprekate <deprekate@gmail.com>
2021-05-21 16:31:46 -0700

    Added ability to allow 1 mismatch

--- find_overlaps.py
+++ find_overlaps.py
@@ -15,12 +15,22 @@ def encode(string):
         result = (result << 2) | ((ord(char) >> 1) & 3)
     return result
 
+bases = ['A','C','T','G']
+def make_perms(s, allow):
+	if not allow:
+		yield s
+	else:
+		for i, c in enumerate(s):
+			for other in bases :
+				yield s[0:i]  + other + s[i+1:]
+
 usage = '%s [-opt1, [-opt2, ...]] file1 file2' % __file__
 parser = argparse.ArgumentParser(description='', formatter_class=RawTextHelpFormatter, usage=usage)
 parser.add_argument('file1', type=is_valid_file, help='input file')
 parser.add_argument('file2', type=is_valid_file, help='input file')
 parser.add_argument('-o', '--outfile', action="store", default=sys.stdout, type=argparse.FileType('w'), help='where to write output [stdout]')
 parser.add_argument('-m', '--min', type=int, default=30, help='minimum length of overlap')
+parser.add_argument('-a', '--allow', action='store_true', help='allow 1 mismatch')
 args = parser.parse_args()
 
 
@@ -47,17 +57,17 @@ with open(args.file1) as fp:
 		if line.startswith('>'):
 			seen = dict()
 			for i in range(len(seq) - args.min + 1):
-				kmer = seq[:len(seq)-i]
-				#bits = encode(seq[:len(seq)-i])
-				if kmer in rights:
-					for right in rights[kmer]:
-						if right not in seen:
-							print(head, '_', right, sep='', end=',')
-							print('1'.rjust(4), end=',')
-							print(str(length[right] - len(kmer) + 1).rjust(4), end=',')
-							print(str(len(kmer)).rjust(4), end=', ')
-							print(kmer)
-						seen[right] = True
+				for kmer in make_perms(seq[:len(seq)-i], args.allow):
+					#bits = encode(seq[:len(seq)-i])
+					if kmer in rights:
+						for right in rights[kmer]:
+							if right not in seen:
+								print(head, '_', right, sep='', end=',')
+								print('1'.rjust(4), end=',')
+								print(str(length[right] - len(kmer) + 1).rjust(4), end=',')
+								print(str(len(kmer)).rjust(4), end=', ')
+								print(kmer)
+							seen[right] = True
 			head = line[1:].rstrip().split(' ')[0]
 			seq = ''
 		else:
