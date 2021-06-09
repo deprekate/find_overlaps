addac7c836328d786e6f123b46c5abe01f5cbd2b
deprekate <deprekate@gmail.com>
2021-06-09 14:25:55 -0700

    Added ability to show entropy of the match

--- find_overlaps.py
+++ find_overlaps.py
@@ -3,6 +3,9 @@ import sys
 import argparse
 from argparse import RawTextHelpFormatter
 from itertools import chain as add_last
+from math import log
+import collections
+import math
 
 def is_valid_file(x):
     if not os.path.exists(x):
@@ -14,6 +17,18 @@ def encode(string):
     for char in string:
         result = (result << 2) | ((ord(char) >> 1) & 3)
     return result
+
+def entropy(sequence, k=4):
+	H = 0
+	counts = dict()
+	for i in range(0, len(sequence)-k+1):
+		seq = sequence[i:i+k]
+		counts[seq] = counts.get(seq, 0) + 1
+	total = sum(counts.values())
+	for count in counts.values():
+		H += (count/total) * log(count/total)		
+	return -H
+
 
 bases = ['A','C','T','G']
 def make_perms(s, allow):
@@ -30,6 +45,7 @@ parser.add_argument('file1', type=is_valid_file, help='input file')
 parser.add_argument('file2', type=is_valid_file, help='input file')
 parser.add_argument('-o', '--outfile', action="store", default=sys.stdout, type=argparse.FileType('w'), help='where to write output [stdout]')
 parser.add_argument('-m', '--min', type=int, default=30, help='minimum length of overlap')
+parser.add_argument('-e', '--entropy', type=int, default=0, help='size of kmers to show entropy for alignment')
 parser.add_argument('-a', '--allow', action='store_true', help='allow 1 mismatch')
 args = parser.parse_args()
 
@@ -66,7 +82,11 @@ with open(args.file1) as fp:
 								print('1'.rjust(4), end=',')
 								print(str(length[right] - len(kmer) + 1).rjust(4), end=',')
 								print(str(len(kmer)).rjust(4), end=', ')
-								print(kmer)
+								print(kmer, end='')
+								if args.entropy:
+									print(', ', end='')
+									print(entropy(kmer, args.entropy), end='')
+								print()
 							seen[right] = True
 			head = line[1:].rstrip().split(' ')[0]
 			seq = ''
