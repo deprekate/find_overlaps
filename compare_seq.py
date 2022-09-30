9f42ac2a006ffa937cec4415a4726ab779c7d7c1
deprekate <deprekate@gmail.com>
2022-09-30 14:50:50 -0700

    Changed flow of compare_seq

--- compare_seq.py
+++ compare_seq.py
@@ -20,6 +20,7 @@ parser.add_argument('directory', type=is_valid_file, help='input file')
 parser.add_argument('-o', '--outfile', action="store", default=sys.stdout, type=argparse.FileType('w'), help='where to write output [stdout]')
 parser.add_argument('-c', '--column', type=int, default=1, help='The column to look for the sequence in')
 parser.add_argument('-x', '--exact', action='store_true', help='Allow only exact matches')
+parser.add_argument('-n', '--number', type=int, default=None, help='The sge index of this processes')
 #parser.add_argument('-r', '--revcomp', action='store_true', help='do reverse complement comparison')
 args = parser.parse_args()
 
@@ -30,27 +31,30 @@ files = sorted( os.listdir(args.directory) )
 args.outfile.write('sequence')
 sequences = dict()
 for name in files:
-	args.outfile.write('\t')
-	args.outfile.write(name)
 	with open(os.path.join(args.directory, name)) as f:
 		for line in f:
 			seq = line.rstrip().split('\t')[args.column - 1]
 			if seq:
 				sequences[seq] = True
-args.outfile.write('\n')
 
+seqs = dict()
+for name in files[args.number-1 : args.number]:
+	args.outfile.write('\t')
+	args.outfile.write(name)
+	path = os.path.join(args.directory, name)
+	with open(path) as f:
+		for line in f:
+			seqs[line.rstrip().split('\t')[args.column - 1]] = True
+
+args.outfile.write('\n')
 # Go THROUGH EACH FILE AND FIND WHETHER THE SEQ IS PRESENT
 for seq in sequences.keys():
 	args.outfile.write(seq)
-	for name in files:
-		with open(os.path.join(args.directory, name)) as f:
-			seen = '0'
-			for line in f:
-				rseq = line.rstrip().split('\t')[args.column - 1]
-				if seq and (not args.exact and seq in rseq) or seq == rseq:
-					seen = '1'
-		args.outfile.write('\t')
-		args.outfile.write(seen)
+	args.outfile.write('\t')
+	if seq in seqs:
+		args.outfile.write('1')
+	else:
+		args.outfile.write('0')
 	args.outfile.write('\n')
 
 
