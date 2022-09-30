9aeab98b6b49ed9aa20e32dc29e91c441b39a9e2
deprekate <deprekate@gmail.com>
2022-09-30 15:14:13 -0700

    Changed flow of compare_seq

--- compare_seq.py
+++ compare_seq.py
@@ -19,8 +19,7 @@ parser = argparse.ArgumentParser(description='', formatter_class=RawTextHelpForm
 parser.add_argument('directory', type=is_valid_file, help='input file')
 parser.add_argument('-o', '--outfile', action="store", default=sys.stdout, type=argparse.FileType('w'), help='where to write output [stdout]')
 parser.add_argument('-c', '--column', type=int, default=1, help='The column to look for the sequence in')
-parser.add_argument('-x', '--exact', action='store_true', help='Allow only exact matches')
-parser.add_argument('-n', '--number', type=int, default=None, help='The sge index of this processes')
+#parser.add_argument('-x', '--exact', action='store_true', help='Allow only exact matches')
 #parser.add_argument('-r', '--revcomp', action='store_true', help='do reverse complement comparison')
 args = parser.parse_args()
 
@@ -32,29 +31,24 @@ args.outfile.write('sequence')
 sequences = dict()
 for name in files:
 	with open(os.path.join(args.directory, name)) as f:
+		args.outfile.write('\t')
+		args.outfile.write(name)
 		for line in f:
 			seq = line.rstrip().split('\t')[args.column - 1]
 			if seq:
-				sequences[seq] = True
-
-seqs = dict()
-for name in files[args.number-1 : args.number]:
-	args.outfile.write('\t')
-	args.outfile.write(name)
-	path = os.path.join(args.directory, name)
-	with open(path) as f:
-		for line in f:
-			seqs[line.rstrip().split('\t')[args.column - 1]] = True
-
+				sequences[seq] = []
+				sequences[seq].append(name)
 args.outfile.write('\n')
-# Go THROUGH EACH FILE AND FIND WHETHER THE SEQ IS PRESENT
-for seq in sequences.keys():
+
+# Go THROUGH THE SEQS OF EACH FILE AND FIND WHETHER THE SEQ IS PRESENT
+for seq,names in sequences.items():
 	args.outfile.write(seq)
-	args.outfile.write('\t')
-	if seq in seqs:
-		args.outfile.write('1')
-	else:
-		args.outfile.write('0')
+	for name in files:
+		args.outfile.write('\t')
+		if name in names:
+			args.outfile.write('1')
+		else:
+			args.outfile.write('0')
 	args.outfile.write('\n')
 
 
